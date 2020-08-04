b843b811d0a2772b80654e97b820c25e819263de
Minkin Aleksey <crazy_genius@mail.ru>
2020-08-04 15:10:57 +1000

    Добавим базовый набор для cli приложения

---
+++ src/Application.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+/**
+ * Class Application
+ * @package App
+ */
+class Application
+{
+    public function run(): int
+    {
+        return 0;
+    }
+}
